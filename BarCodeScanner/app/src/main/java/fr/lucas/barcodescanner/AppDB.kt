629e3d68150c596474a8f2988526ee00e442335a
Ryodor <selatniryadg@gmail.com>
2019-11-15 13:27:21 +0100

    Systeme de stockage interne version exemple
    J'ai suivie un tuto qui ma permis de comprendre et appliquer le principe de Room qui émule du sqlite

---
+++ BarCodeScanner/app/src/main/java/fr/lucas/barcodescanner/AppDB.kt
@@ -0,0 +1,11 @@
+package fr.lucas.barcodescanner
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database (entities = [ (Emp_Entity::class)], version = 1 )
+abstract class AppDB : RoomDatabase () {
+
+    abstract fun empDAO() : Emp_DAO
+
+}
--- BarCodeScanner/app/src/main/java/fr/lucas/barcodescanner/MainActivity.kt
+++ BarCodeScanner/app/src/main/java/fr/lucas/barcodescanner/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.*
 import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.activity_main.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
@@ -23,6 +24,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.nio.charset.CodingErrorAction.REPLACE
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +35,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        takePhoto.setOnClickListener{
+        var db = Room.databaseBuilder(applicationContext, AppDB::class.java, "EmployeeDB").build()
+
+        Thread {
+            var emp = Emp_Entity()
+            emp.emp_id = 2
+            emp.emp_name = "Lulu"
+            emp.emp_post = "admin6"
+
+            db.empDAO().saveEmp(emp)
+            db.empDAO().readEmp().forEach{
+                Log.i("@AKTDEV", "Id is : ${it.emp_id}" )
+                Log.i("@AKTDEV", "Name is : ${it.emp_name}" )
+                Log.i("@AKTDEV", "Post is : ${it.emp_post}" )
+            }
+        }.start()
+
+
+
+
+        takePhoto.setOnClickListener {
             //viewModel = MainViewModel()
             //viewModel.openCamera(this)
 
@@ -64,7 +85,6 @@ class MainActivity : AppCompatActivity() {
             })*/
             //var intent = Intent(this, CameraActivity::class.java)
             //startActivity(intent)
-            var dialog = FireMissilesDialogFragment()
             //dialog.show()
 
         }
@@ -80,25 +100,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }*/
+
+
 }
 
-class FireMissilesDialogFragment : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            // Use the Builder class for convenient dialog construction
-            val builder = AlertDialog.Builder(it)
-            builder.setMessage("aaaaaaaa")
-                .setPositiveButton("bbbbbb",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // FIRE ZE MISSILES!
-                    })
-                .setNegativeButton("cccccc",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
-                    })
-            // Create the AlertDialog object and return it
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
-    }
-}
